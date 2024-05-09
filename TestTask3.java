a9955ace06268e748b0f8dc24113f6b0de99eee3
Zarina Tazetdinova <maritimiss@gmail.com>
2024-05-10 00:05:17 +0200

    Solved Task 3

---
+++ TestTask3.java
@@ -0,0 +1,15 @@
+public class TestTask3 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        evenNumbers(arr);
+    }
+
+    static void evenNumbers(int[] arr) {
+        System.out.println("Четные числа массива:");
+        for (int i : arr) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
